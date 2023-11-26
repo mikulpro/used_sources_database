e85160615f12bf0e2c721cff0ee394fbb5598dc8
Prokop Mikulasek <prokop12@volny.cz>
2023-11-26 16:52:57 +0100

    Very basic DB interface

--- app.py
+++ app.py
@@ -3,13 +3,11 @@ import sqlite3
 
 app = Flask("lorem")
 
-# Database setup
 def get_db_connection():
     conn = sqlite3.connect('books.db')
     conn.row_factory = sqlite3.Row
     return conn
 
-# Route for the index page
 @app.route('/')
 def index():
     conn = get_db_connection()
@@ -17,21 +15,26 @@ def index():
     conn.close()
     return render_template('index.html', books=books)
 
-# Route for adding a new book
 @app.route('/add', methods=('GET', 'POST'))
 def add():
     if request.method == 'POST':
-        title = request.form['title']
-        author = request.form['author']
-
-        conn = get_db_connection()
-        conn.execute('INSERT INTO books (title, author) VALUES (?, ?)',
-                     (title, author))
-        conn.commit()
-        conn.close()
+        # process form data to add a new book
+        # redirect to index page after adding
         return redirect(url_for('index'))
 
     return render_template('add.html')
+
+@app.route('/edit/<int:id>', methods=('GET', 'POST'))
+def edit(id):
+    # fetch book with the given id and process form data to edit
+    # redirect to index page after editing
+    return render_template('edit.html', book=book)
+
+@app.route('/delete/<int:id>', methods=('POST',))
+def delete(id):
+    # delete the book with the given id
+    # redirect to index page after deleting
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
