db82aab1ad42788361706a4b1bffe8119fe70861
Prokop Mikulasek <prokop12@volny.cz>
2023-11-26 16:43:46 +0100

    Very first html DB interface

--- app.py
+++ app.py
@@ -1,9 +1,37 @@
-from flask import Flask
-app = Flask("cus")
+from flask import Flask, request, render_template, redirect, url_for
+import sqlite3
 
+app = Flask("lorem")
+
+# Database setup
+def get_db_connection():
+    conn = sqlite3.connect('books.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+# Route for the index page
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    conn = get_db_connection()
+    books = conn.execute('SELECT * FROM books').fetchall()
+    conn.close()
+    return render_template('index.html', books=books)
+
+# Route for adding a new book
+@app.route('/add', methods=('GET', 'POST'))
+def add():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+
+        conn = get_db_connection()
+        conn.execute('INSERT INTO books (title, author) VALUES (?, ?)',
+                     (title, author))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
---
+++ schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE books (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    author TEXT NOT NULL
+);
