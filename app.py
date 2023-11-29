a35cf4ddf56adf7cf35ba0ee094c271a6d7f6358
Prokop Mikulasek <prokop12@volny.cz>
2023-11-29 18:18:41 +0100

    Added: Untested GET and PUT methods

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Api, Resource, reqparse
 import sqlite3
 
@@ -35,25 +35,69 @@ api.add_resource(TodoSimple, '/<string:todo_id>')   #
 #####################################################
 
 class InformationSource(Resource):
+    def get(self):
+        id = request.args.get('id')  # Get id from query parameter
+        author = request.args.get('author')  # Get author from query parameter
+        name = request.args.get('name')
+        year = request.args.get('year')
 
-    def get(self, id=None, name=None, author=None, year=None):
         conn = get_db_connection()
-        query = "SELECT * FROM books"
-        all_books = conn.execute(query, (id,)).fetchall()
-        result = []
-        for book in all_books:
-            if book["id"] == id:
-                result.append(book)
-            elif book["name"] == name:
-                result.append(book)
-            elif book["author"] == author:
-                result.append(book)
-            elif book["year"] == year:
-                result.append(book)
-        if result == []:
-            result = "No books found", 204        
-        return result, 200
-api.add_resource(InformationSource, '/bookid/<int:id>', '/bookname/<string:name>', '/bookauth/<string:author>', '/bookyear/<int:year>')
+        cursor = conn.cursor()
+
+        if id:
+            # Fetch books by the specified author
+            cursor.execute('SELECT * FROM books WHERE id = ?', (id,))
+        elif author:
+            # Fetch books by the specified author
+            cursor.execute('SELECT * FROM books WHERE author = ?', (author,))
+        elif name:
+            # Fetch books by the specified author
+            cursor.execute('SELECT * FROM books WHERE name = ?', (name,))
+        elif year:
+            # Fetch books by the specified author
+            cursor.execute('SELECT * FROM books WHERE year = ?', (year,))
+
+        books = cursor.fetchall()
+        conn.close()
+
+        # Convert each row to a dictionary
+        books_list = [dict(ix) for ix in books]
+        
+        return jsonify(books_list)
+
+    def put(self):
+        data = request.json  # Assuming the data is provided in JSON format in the request body
+
+        # Validate that the required fields are present in the JSON data
+        if 'id' not in data:
+            return {'error': 'Missing "id" field in JSON data'}, 400
+
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        # Check if the book with the provided ID exists
+        cursor.execute('SELECT * FROM books WHERE id = ?', (data['id'],))
+        existing_book = cursor.fetchone()
+
+        if not existing_book:
+            conn.close()
+            return {'error': 'Book not found with the provided ID'}, 404
+
+        # Update the book information with the new data
+        cursor.execute(
+            'UPDATE books SET author = ?, name = ?, year = ? WHERE id = ?',
+            (data.get('author', existing_book['author']),
+             data.get('name', existing_book['name']),
+             data.get('year', existing_book['year']),
+             data['id'])
+        )
+
+        conn.commit()
+        conn.close()
+
+        return {'message': 'Book updated successfully'}, 200
+
+api.add_resource(InformationSource, '/books')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
