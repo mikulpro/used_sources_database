4fa65646722253fc87ccdebcd50bd428b772c791
Prokop Mikulasek <prokop12@volny.cz>
2023-12-07 07:44:37 +0100

    UNTESTED updated get and put methods

--- app.py
+++ app.py
@@ -36,28 +36,14 @@ api.add_resource(TodoSimple, '/<string:todo_id>')   #
 
 class InformationSource(Resource):
     def get(self):
-        id = request.args.get('id')  # Get id from query parameter
-        author = request.args.get('author')  # Get author from query parameter
-        name = request.args.get('name')
-        year = request.args.get('year')
-
+        query_string = request.args.get() # Get query - hopefully
         conn = get_db_connection()
         cursor = conn.cursor()
-
-        if id:
-            # Fetch books by the specified author
-            cursor.execute('SELECT * FROM books WHERE id = ?', (id,))
-        elif author:
-            # Fetch books by the specified author
-            cursor.execute('SELECT * FROM books WHERE author = ?', (author,))
-        elif name:
-            # Fetch books by the specified author
-            cursor.execute('SELECT * FROM books WHERE name = ?', (name,))
-        elif year:
-            # Fetch books by the specified author
-            cursor.execute('SELECT * FROM books WHERE year = ?', (year,))
-
-        books = cursor.fetchall()
+        books = cursor.execute('SELECT * FROM books WHERE id = ?', (query_string,))
+        books.append(cursor.execute('SELECT * FROM books WHERE author = ?', (query_string,)))
+        books.append(cursor.execute('SELECT * FROM books WHERE title = ?', (query_string,)))
+        books.append(cursor.execute('SELECT * FROM books WHERE year = ?', (query_string,)))
+        books.append(cursor.execute('SELECT * FROM books WHERE type = ?', (query_string,)))
         conn.close()
 
         # Convert each row to a dictionary
@@ -71,6 +57,14 @@ class InformationSource(Resource):
         # Validate that the required fields are present in the JSON data
         if 'id' not in data:
             return {'error': 'Missing "id" field in JSON data'}, 400
+        if 'title' not in data:
+            return {'error': 'Missing "title" field in JSON data'}, 400
+        if 'author' not in data:
+            return {'error': 'Missing "author" field in JSON data'}, 400
+        if 'type' not in data:
+            return {'error': 'Missing "type" field in JSON data'}, 400
+        if 'year' not in data:
+            return {'error': 'Missing "year" field in JSON data'}, 400
 
         conn = get_db_connection()
         cursor = conn.cursor()
@@ -79,23 +73,37 @@ class InformationSource(Resource):
         cursor.execute('SELECT * FROM books WHERE id = ?', (data['id'],))
         existing_book = cursor.fetchone()
 
-        if not existing_book:
-            conn.close()
-            return {'error': 'Book not found with the provided ID'}, 404
-
-        # Update the book information with the new data
-        cursor.execute(
+        if existing_book:
+            cursor.execute(
             'UPDATE books SET author = ?, name = ?, year = ? WHERE id = ?',
             (data.get('author', existing_book['author']),
              data.get('name', existing_book['name']),
              data.get('year', existing_book['year']),
              data['id'])
-        )
+            )
+            conn.commit()
+            conn.close()
+            return {'message': 'Book updated successfully'}, 200
+        else:
+            cursor.execute(
+            'INSERT INTO books (id, title, author, type, year) VALUES (?, ?, ?, ?, ?)',
+            (data['id'],
+             data['title'],
+             data['author'],
+             data['type'],
+             data['year'])
+            )
+            conn.commit()
+            conn.close()    
+            return {'message': 'New book inserted successfully'}, 201
+        
+        def head(self):
+            #TODO:
+            ...
 
-        conn.commit()
-        conn.close()
-
-        return {'message': 'Book updated successfully'}, 200
+        def trace(self):
+            #TODO:
+            ...
 
 api.add_resource(InformationSource, '/books')
 
