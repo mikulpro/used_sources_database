bde08160066885eb0eb474300c4cf088840a61b4
Prokop Mikulasek <prokop12@volny.cz>
2024-01-14 22:10:32 +0100

    UNTESTED Added automatized testing script

---
+++ testing/automatised_testing.py
@@ -0,0 +1,33 @@
+import sys
+import requests
+import json
+
+URL = 'http://127.0.0.1:5000/'
+
+TO_TEST = [
+    [f"{URL}book", "POST", "testing/single_book.json"],
+    [f"{URL}book", "PUT", "testing/edited_book.json"],
+    [f"{URL}book?id=1", "GET", ""],
+    [f"{URL}book?id=1", "DELETE", ""]
+]
+
+def main():
+    for test in TO_TEST:
+        if test[2] is None:
+            try:
+                response = getattr(requests, test[1].lower())(test[0], data="")
+                print(response.text)
+            except Exception as e:
+                print(str(e))
+        else:
+            try:
+                data = {}
+                with open(test[2], 'r') as file:
+                    data = json.load(file)
+                response = getattr(requests, test[1].lower())(test[0], json=data)
+                print(response.text)
+            except Exception as e:
+                print(str(e))
+
+if __name__ == '__main__':
+    main()
