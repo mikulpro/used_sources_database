85966454cc399276fa3b309ae039c6315adc6f61
doctordehi <doctordehi@gmail.com>
2024-02-05 01:24:46 +0100

    smazané staré relikvie po staré db

--- utils/utils.py
+++ utils/utils.py
@@ -1,14 +1,4 @@
 # utils.py
-import sqlite3
-
-from config import DATABASE
-
-
-def get_db_connection():
-    conn = sqlite3.connect(DATABASE)
-    conn.row_factory = sqlite3.Row
-    return conn
-
 
 def is_integer(s):
     try:
